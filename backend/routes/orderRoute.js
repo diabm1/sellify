c65b6fe8e2bc817a338fb78f34f78a03f425b76b
olacase@gmail.com <olacase@gmail.com>
2022-07-07 16:34:37 -0500

    backend fixes

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -53,7 +53,7 @@ router.put("/:id/pay", isAuth, async (req, res) => {
     order.isPaid = true;
     order.paidAt = Date.now();
     order.payment = {
-      paymentMethod: 'paypal',
+      // paymentMethod: 'paypal',
       paymentResult: {
         payerID: req.body.payerID,
         orderID: req.body.orderID,
