41d3c4ed5d1ea728d0e5308579db517a7eeafe6e
Josiah Douglass <josiahdouglass64@gmail.com>
2022-07-07 14:57:40 -0400

    added sample cards

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -3,12 +3,32 @@ import React from "react";
 function Products() {
 
     return (
-        <div>
+        <div className="container">
             <h1>Product</h1>
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap"/>
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className="row">
+                <div className="card" style="width: 18rem;">
+                    <img className="card-img-top" src="frontend\public\logo512.png" alt="Card image cap"/>
+                    <div className="card-body">
+                        <p className="card-text">Pants</p>
+                    </div>
+                </div>
+                <div className="card" style="width: 18rem;">
+                    <img className="card-img-top" src="frontend\public\logo512.png" alt="Card image cap"/>
+                    <div className="card-body">
+                        <p className="card-text">Socks</p>
+                    </div>
+                </div>
+                <div className="card" style="width: 18rem;">
+                    <img className="card-img-top" src="frontend\public\logo512.png" alt="Card image cap"/>
+                    <div className="card-body">
+                        <p className="card-text">Artificially Flavored Handsoap</p>
+                    </div>
+                </div>
+                <div className="card" style="width: 18rem;">
+                    <img className="card-img-top" src="frontend\public\logo512.png" alt="Card image cap"/>
+                    <div className="card-body">
+                        <p className="card-text">Ribbed for her pleasure coffee machines</p>
+                    </div>
                 </div>
             </div>
         </div>
