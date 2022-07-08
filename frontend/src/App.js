8b7ecd9bdacf7729e41e0a63abac98e6a8fc470e
olacase@gmail.com <olacase@gmail.com>
2022-07-08 18:41:46 -0500

    restructured but react is still not displaying

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,100 +1,42 @@
-import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import '../src/App.css';
-import CartView from './views/cartView';
-
-import HomeView from './views/homeView';
-import ProductView from './views/productView';
-import Signin from './views/signIn';
-import { useSelector } from 'react-redux';
-import Register from './views/Register';
-import ProductsView from './views/productsView';
-
-import ShippingView from './views/shippingView';
-import PaymentView from './views/paymentView';
-// import PlaceOrder from './views/PlaceOrder';
-import Orders from './views/Orders';
-import ProfileView from './views/profileView';
-// import HomeView from './views/homeView';
-// import ProductView from './views/productView';
-// import ProductsView from './views/productsView';
-
-
+import React, { useState } from "react";
+import Header from "./components/Header";
+import { Routes, Route, Navigate, } from "react-router-dom";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+// import Products from "./components/Products";
+import { DataProvider } from "./components/DataProvider";
+// import Details from "./components/Details/Details";
+// import Cart from "./components/Cart";
+// import Register from "./components/Signup/Register";
+// import Error from "./components/Error";
 
 function App() {
-  const userSignin = useSelector((state) => state.userSignin);
-  const { userInfo } = userSignin;
 
-  const openMenu = () => {
-    document.querySelector('.sidebar').classList.add('open');
-  };
-  const closeMenu = () => {
-    document.querySelector('.sidebar').classList.remove('open');
-  };
+  const [search, setSearch] = useState("");
+
   return (
-    <BrowserRouter>
-      <div className="grid-container">
-        <header className="header">
-          <div className="brand">
-            <button onClick={openMenu}>&#9776;</button>
-            <Link to="/">Sellify</Link>
-          </div>
-          <div className="header-links">
-            <a href="cart.html">Cart</a>
-            {userInfo ? (
-              <Link to="/profile">{userInfo.name}</Link>
-            ) : (
-              <Link to="/signin">Sign In</Link>
-            )}
-            {userInfo && userInfo.isAdmin && (
-              <div className="dropdown">
-                <a href="#">Admin</a>
-                <ul className="dropdown-content">
-                  <li>
-                    <Link to="/orders">Orders</Link>
-                    <Link to="/products">Products</Link>
-                  </li>
-                </ul>
-              </div>
-            )}
-          </div>
-        </header>
-        <aside className="sidebar">
-          <h3>Shopping Categories</h3>
-          <button className="sidebar-close-button" onClick={closeMenu}>
-            x
-          </button>
-          <ul className="categories">
-            <li>
-              <Link to="/category/Pants">Pants</Link>
-            </li>
+    <DataProvider>
+      
+              <Routes>
+          <Route exact path="/error404" element={Error} />
+          <Route>
 
-            <li>
-              <Link to="/category/Shirts">Shirts</Link>
-            </li>
-          </ul>
-        </aside>
-        <main className="main">
-          <div className="content">
-            <Route path='/cart/:id?' component ={CartView} />
-            <Route path="/Orders" component={Orders} />
-            <Route path="/profile" component={ProfileView} />
-            {/* <Route path="/PlaceOrder/:id" component={placeOrder} /> */}
-            <Route path="/products" component={ProductsView} />
-            <Route path="/shipping" component={ShippingView} />
-            <Route path="/payment" component={PaymentView} />
-            {/* <Route path="/placeorder" component={PlaceOrder} /> */}
-            <Route path="/signin" component={Signin} />
-            <Route path="/register" component={Register} />
-            <Route path="/product/:id" component={ProductView} />
-            <Route path="/category/:id" component={HomeView} />
-            <Route path="/" exact={true} component={HomeView} />
-        
-          </div>
-        </main>
-        <footer className="footer">All right reserved.</footer>
-      </div>
-    </BrowserRouter>
+            <Header />
+            <Route>
+              <Route exact path="/" component={Home} />
+              {/* <Route exact path="/products" component={() => <Products search={search} />} /> */}
+              {/* <Route exact path="/products/:id" component={Details} /> */}
+              {/* <Route exact path="/cart" component={Cart} /> */}
+              {/* <Route exact path="/register" component={Register} /> */}
+              <Navigate to="/error404" />
+              {/* <Products /> */}
+            </Route>
+            <Footer />
+            
+          </Route>
+        </Routes>
+    
+     </DataProvider>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import {Provider} from 'react-redux'
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
+
+// import './dist/css/main.css';
+// import { HashRouter } from 'react-router-dom';
+
 import reportWebVitals from './reportWebVitals';
-import store from './store';
 
 ReactDOM.render(
-
- <Provider store={store}>
-<App />
-  </Provider>, document.getElementById('root')
-    
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
