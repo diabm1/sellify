ddffb9260ee617514426b893face87db183d106f
diabm1 <diabm1855@gmail.com>
2022-07-12 20:26:25 -0400

    commiting changes before develop

--- frontend/src/Pages/SignInPage.js
+++ frontend/src/Pages/SignInPage.js
@@ -1,95 +1,29 @@
-<<<<<<< HEAD:frontend/src/screens/SigninScreen.js
 import React, { useEffect, useState } from "react";
-import { Link, useSearchParams, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { signin } from "../actions/userActions";
 
-function SigninScreen(props) {
+function SignInPage(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const userSignin = useSelector((state) => state.userSignin);
-=======
-
-
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { signin } from '../actions/userActions';
-
-function SignInPage(props) {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const userSignin = useSelector(state => state.userSignin);
->>>>>>> develop:frontend/src/Pages/SignInPage.js
   const { loading, userInfo, error } = userSignin;
   const dispatch = useDispatch();
-  const search = useSearchParams();
+  // const search = useSearchParams();
   const navigate = useNavigate();
+  const redirect = props.location.search ? props.location.search.split("=")[1] : '/';
   useEffect(() => {
     if (userInfo) {
-      navigate("/profile");
+      navigate(redirect);
     }
     return () => {
       //
     };
-  }, [userInfo]);
+  }, [navigate, redirect, userInfo]);
 
   const submitHandler = (e) => {
     e.preventDefault();
     dispatch(signin(email, password));
-<<<<<<< HEAD:frontend/src/screens/SigninScreen.js
-  };
-  return (
-    <div className="form">
-      <form onSubmit={submitHandler}>
-        <ul className="form-container">
-          <li>
-            <h2>Sign-In</h2>
-          </li>
-          <li>
-            {loading && <div>Loading...</div>}
-            {error && <div>{error}</div>}
-          </li>
-          <li>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
-          </li>
-          <li>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
-          </li>
-          <li>
-            <button type="submit" className="button primary">
-              Signin
-            </button>
-          </li>
-          <li>New to Sellify?</li>
-          <li>
-            <Link
-              to={search === "/" ? "register" : "register?redirect=" + search}
-              className="button secondary text-center"
-            >
-              Create your amazona account
-            </Link>
-          </li>
-        </ul>
-      </form>
-    </div>
-  );
-}
-export default SigninScreen;
-=======
 
   }
   return <div className="form">
@@ -128,4 +62,3 @@ export default SigninScreen;
   </div>
 }
 export default SignInPage;
->>>>>>> develop:frontend/src/Pages/SignInPage.js
