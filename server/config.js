1ad9a93c63f96209483fbf35a15d354a89be060e
diabm1 <diabm1855@gmail.com>
2022-07-14 03:00:33 -0400

    testing seeds

--- server/config.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sellify', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = mongoose.connection;
