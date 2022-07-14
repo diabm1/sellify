d24bbe12de3553c15e70ce63988b3dfb18631583
diabm1 <diabm1855@gmail.com>
2022-07-14 01:30:07 -0400

    added index.js

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require("./userModel");
+const Product = require("./productModel");
+const Category = require("./categoryModel");
+const Order = require("./orderModel");
+
+module.exports = { User, Product, Category, Order };
